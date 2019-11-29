7b2e2ea024ea7185a4725fca64afc12ce1835068
dodinhnguyen <nguyendo891@yahoo.com>
2019-11-29 16:29:02 +0100

    Add project files.

---
+++ Creational.Singleton.Monostate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Singleton.Monostate
+{
+    public class ChiefExecutiveOfficer
+    {
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var ceo = new ChiefExecutiveOfficer();
+            ceo.Name = "Adam Smith";
+            ceo.Age = 55;
+
+            var ceo2 = new ChiefExecutiveOfficer();
+            Console.WriteLine(ceo2);
+        }
+    }
+}
